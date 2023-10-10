a6f6800929f86550b84b3c50ea1a022e3ee1b5e7
Hritvik <147504582+hritvik2@users.noreply.github.com>
2023-10-10 20:47:54 +0530

    base version

---
+++ cal.c
@@ -0,0 +1,49 @@
+# include<stdio.h>
+
+int main(){
+fflush(stdin);
+int c,b;
+char e;
+printf("Select symbol\n");
+scanf("%c",&e);
+if (e =='a')
+{
+
+    printf("Tell the value of first number");
+    fflush(stdin);
+   scanf("%d",&c);
+   printf("Tell the value of second number");
+   fflush(stdin);
+   scanf("%d",&b); 
+   printf("Here is addition%d",c+b);
+}
+else if (e =='s')
+{
+    
+
+    printf("Tell the value of first number");
+    fflush(stdin);
+   scanf("%d",&c);
+   printf("Tell the value of second number");
+   fflush(stdin);
+   scanf("%d",&b); 
+    
+    printf("Here is addition%d",c-b);
+}
+
+else if (e =='m')
+{
+
+ 
+    printf("Tell the value of first number");
+    fflush(stdin);
+   scanf("%d",&c);
+   printf("Tell the value of second number");
+   fflush(stdin);
+   scanf("%d",&b); 
+    
+    printf("Here is addition%d",c*b);
+}
+return 0;
+
+}
